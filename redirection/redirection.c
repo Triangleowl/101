3b35e578eaf194803e2209435c96409c0fd0dd25
owl <45646343+owl207@users.noreply.github.com>
2019-09-11 21:03:18 +0800

    Add files via upload

---
+++ redirection/redirection.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void foo(void){
+    int a;
+    int fd = open("input", O_RDWR);
+    dup2(fd, 0);
+    scanf("%d", &a);
+
+    printf("a is: %d\n", a);
+}
+
+
+int main(){
+
+    foo();
+    return 0;
+}
